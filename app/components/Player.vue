47e1bda3250450829b933220a5d9d418d5c66d6c
SHatice <sarah.gungor@hetic.net>
2019-06-25 11:31:03 +0200

    add video to player

--- app/components/Player.vue
+++ app/components/Player.vue
@@ -1,14 +1,14 @@
 <template>
 <div class="player">
+  <vue-plyr>
+    <div class="plyr__video-embed">
+      <iframe
+        src="https://player.vimeo.com/video/344272014?loop=false&byline=false&portrait=false&title=false&speed=true&transparent=0&gesture=media&autoplay=true"
+        allowfullscreen autoplay allowtransparency allow="autoplay">
+      </iframe>
+    </div>
+  </vue-plyr>
   <section class="player__info">
-    <vue-plyr>
-      <div class="plyr__video-embed">
-        <iframe
-          src="https://player.vimeo.com/video/76979871?loop=false&byline=false&portrait=false&title=false&speed=true&transparent=0&gesture=media&autoplay=true"
-          allowfullscreen autoplay allowtransparency allow="autoplay">
-        </iframe>
-      </div>
-    </vue-plyr>
     <div class="info__goBack">
       <svg v-on:click="closeMenu()" class="arrow" aria-hidden="true"><use xlink:href="#arrow"></use></svg>
       <p>Continuer la lecture</p>
@@ -32,11 +32,6 @@
   .plyr {
     width: 100vw;
     height: 100vh;
-
-    iframe {
-      width: 100%;
-      height: 100%;
-    }
   }
 </style>
 
@@ -75,5 +70,7 @@ export default {
       menu.classList.remove('is-open');
       playerinfo.classList.remove('is-open');
     }
+  }
 };
+
 </script>
