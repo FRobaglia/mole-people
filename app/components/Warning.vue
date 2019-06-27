23a0624cbdf504eceb299dbf24d877debaad76d1
SHatice <sarah.gungor@hetic.net>
2019-06-27 21:10:16 +0200

    change warning section css

--- app/components/Warning.vue
+++ app/components/Warning.vue
@@ -10,48 +10,7 @@
 </template>
 
 <style lang="scss" scoped>
-  .whole-page {
-    padding-top: 50px;
-    width: 100vw;
-    height: 100vh;
-    background: #080B22 !important; 
-    
-    .big {
-      font-size: 38px;
-      font-weight: bold;
-    }
-
-    .start-doc {
-      transition: transform .2s;
-      display: block;
-      padding: 12px;
-      width: 10%;
-      text-align: center;
-      text-decoration: none;
-      margin: 0 auto;
-      margin-top: 50px;
-      color: black;
-      border-radius: 4px;
-      background: white;
-
-      &:hover {
-        transform: scale(0.9) translateY(4px);
-      }
-    }
-    p {
-      font-size: 21px;
-      font-weight: normal;
-      text-align: center;
-    }
-
-    img {
-      width: 15vw;
-      margin: 0 auto;
-      margin-bottom: 50px;
-      display: block;
-      filter: invert(100%);
-    }
-  }
+ 
 </style>
 
 <script>
